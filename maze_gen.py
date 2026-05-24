2f7a6392e12de98df9eed779c14c1adcca2a7617
Nare Hakobyan <narhakob@c2r11s1.42yerevan.am>
2026-05-24 20:37:12 +0400

    made cell and matrix

---
+++ maze_gen.py
@@ -0,0 +1,31 @@
+import random
+
+
+class Cell:
+    def __init__(self):
+        self.walls = 0xF #fully closed
+
+    def has_wall(self, direction: int) -> bool:
+        return bool(self.walls & direction)
+    
+    def remove_wall(self, direction: int) -> None:
+        self.walls &= ~direction
+    
+    def add_wall(self, direction: int) -> None:
+        self.walls |= direction
+
+class Grid:
+    def __init__(self, width: int, height: int) -> None:
+        self.width = width
+        self. height = height
+        self.cells = list[list[Cell]] = []
+
+        for y in range(height):
+            row: list[Cell] = []
+            for x in range(width):
+                row.append(Cell())
+            self.cells.append(row)
+
+    def get(self, x: int, y: int) -> Cell:
+        return self.cells[y][x]
+    
